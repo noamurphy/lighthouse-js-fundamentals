c28b824e0e26835b7c188a9747ae53a9a8cd04f5
Noah Murphy <ncmurphy27@gmail.com>
2022-08-20 18:57:12 -0300

    kitchen packing list iterated with while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+//For loop
+/*
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+//While loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
